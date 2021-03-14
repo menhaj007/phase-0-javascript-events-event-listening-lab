657f142bb13049931aa39ce6ca9d2ed4c5450b54
menhaj <menhaj007@yahoo.com>
2021-03-13 19:05:12 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
+    input.addEventListener("click", function(){
+        alert('I was clicked!');
+    });
+    
 }
